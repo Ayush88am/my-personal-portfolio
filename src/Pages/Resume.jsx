c1e85865d6fcbd1c24cf535e66f78bd1583128b8
Ayush mishra <ayushmishraam8@gmail.com>
2024-10-05 16:47:01 +0530

    error fix

--- src/Pages/Resume.jsx
+++ src/Pages/Resume.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import BioContentResume from '../Components/BioContentResume'
 import SectionImage from '../Components/SectionImage'
-import Experience from '../Components/experience'
+import Experience from '../Components/Experience'
 import ContactInput from '../Components/ContactInput'
 const Resume = () => {
   return (
